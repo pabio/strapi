d1463754ad9171f87db832ead61d48d7dc796c4a
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-24 22:54:50 +0530

    :tada: Init Strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1249581570a559c99ca00292f4b505af'),
+    },
+  },
+});
