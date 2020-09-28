9bf768dfa83f1616da052de237d2b2ab53979ef0
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-28 21:30:35 +0530

    :sparkles: Add support articles category

---
+++ api/support/models/support.js
@@ -0,0 +1,20 @@
+"use strict";
+const cryptoRandomString = require("crypto-random-string");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.slug = cryptoRandomString();
+      }
+    },
+    beforeUpdate: async (_, data) => {
+      data.slug = data.slug || cryptoRandomString();
+    },
+  },
+};
