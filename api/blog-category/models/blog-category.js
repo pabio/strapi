616b5ec6540322d41cc59e3122308be7e7e7635d
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-28 21:37:04 +0530

    :recycle: Don't overwrite slug

--- api/blog-category/models/blog-category.js
+++ api/blog-category/models/blog-category.js
@@ -11,7 +11,7 @@ module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title) {
-        data.slug = slugify(data.title);
+        data.slug = data.slug || slugify(data.title);
       }
     },
     beforeUpdate: async (_, data) => {
--- api/support/models/support.js
+++ api/support/models/support.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const cryptoRandomString = require("crypto-random-string");
 
 /**
